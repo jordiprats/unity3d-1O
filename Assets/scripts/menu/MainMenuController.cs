4fcb59f6a1df958f77a26d1a02b3e77f5300ccca
Jordi Prats <jprats@systemadmin.es>
2018-01-06 18:10:32 +0100

    actions menu

---
+++ Assets/scripts/menu/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void QuitAction()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void PlayAction()
+    {
+        Debug.Log("play!")
+        SceneManager.LoadScene("1setembre");
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # unity3d-1O
 
 gitignore: https://gist.github.com/FullStackForger/20bbf62861394b1a3de0
+
+## missions
+
+* 1 de setembre: recollir les urnes
+...
+* 30 de setembre: deixar les urnes aprop del colegi
+* 1 d'octubre: defensar el colegi
