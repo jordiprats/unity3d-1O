e9d89aee6caf4be839aafa81b973439c842fa260
Jordi Prats <jprats@systemadmin.es>
2018-01-07 22:32:44 +0100

    començant camera controller

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float yaw;
+    public float pitch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
