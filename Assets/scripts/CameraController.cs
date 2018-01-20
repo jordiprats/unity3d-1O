4ad8738690c9ab668c58e43895e0bfa7690e2ee6
Jordi Prats <jprats@systemadmin.es>
2018-01-20 09:46:25 +0100

    3rd person camera + moviemnt

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public float yaw;
-    public float pitch;
+    private const float Y_ANGLE_MIN = -5.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+    public float distance = 10.0f;
+    public float currentX = 0.0f;
+    public float currentY = 0.0f;
+    public float sensivityX = 4.0f;
+    public float sensivityY = 1.0f;
+
+    public void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+
+
+    }
+
+    public void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    public void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+        camTransform.position = lookAt.position + rotation * dir;
+
+        camTransform.LookAt(lookAt.position);
+    }
 }
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpforce = 8.0f;
+    public float gravity = 10.0f;
+    private Vector3 moveDir = Vector3.zero;
+	
+	void Update () {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+        if(controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            moveDir = transform.TransformDirection(moveDir);
+
+            moveDir *= speed;
+
+            if(Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpforce;
+            }
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDir * Time.deltaTime);
+	}
+}
