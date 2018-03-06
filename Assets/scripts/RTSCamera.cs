8980e9d415d332d1a717d18cb00a1a0bbe2af7f6
jordiprats <jordi.prats@gmail.com>
2018-03-06 22:29:31 +0100

    work in progress, unit selection

---
+++ Assets/scripts/RTSCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSCamera : MonoBehaviour {
+
+	public float panSpeed = 20f;
+	public float minPanSpeed = 5f;
+	public float panBorderThickness = 50f;
+	public Vector2 panLimit;
+	public Vector2 scrollLimit;
+	public float scrollSpeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 new_pos = transform.position;
+
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		if(scroll!=0)
+		{
+			RaycastHit hit;
+			Ray ray = this.transform.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			Vector3 desiredPosition;
+
+			if (Physics.Raycast(ray , out hit))
+				desiredPosition = hit.point;
+			else
+				desiredPosition = transform.position;
+			float distance = Vector3.Distance(desiredPosition , transform.position);
+			Vector3 direction = Vector3.Normalize( desiredPosition - transform.position) * (distance * scroll * scrollSpeed);
+
+			new_pos += direction;
+			if(new_pos.x < panLimit.x && new_pos.x > -panLimit.x && new_pos.y < scrollLimit.y && new_pos.y > scrollLimit.x && new_pos.z < panLimit.y && new_pos.z > -panLimit.y)
+				transform.position = new_pos;
+			else
+				new_pos = transform.position;
+		}
+
+		float zoom_factor = (new_pos.y-scrollLimit.x)/(scrollLimit.y-scrollLimit.x);
+		//Debug.Log("zoom_factor: "+zoom_factor);
+
+		//Debug.Log("mouse x: "+Input.mousePosition.x+" vs "+(Screen.height - panBorderThickness));
+		//Debug.Log("mouse y: "+Input.mousePosition.y+" vs "+(Screen.width - panBorderThickness));
+		if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+		{
+			new_pos.z += (minPanSpeed + panSpeed * zoom_factor) * Time.deltaTime;
+		}
+
+		if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+		{
+			new_pos.z -= (minPanSpeed + panSpeed * zoom_factor) *  Time.deltaTime;
+		}
+
+		if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+		{
+			new_pos.x += (minPanSpeed + panSpeed * zoom_factor) * Time.deltaTime;
+		}
+
+		if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+		{
+			new_pos.x -= (minPanSpeed + panSpeed * zoom_factor) * Time.deltaTime;
+		}
+
+		new_pos.x = Mathf.Clamp(new_pos.x, -panLimit.x, panLimit.x);
+		new_pos.y = Mathf.Clamp(new_pos.y, scrollLimit.x, scrollLimit.y);
+		new_pos.z = Mathf.Clamp(new_pos.z, -panLimit.y, panLimit.y);
+
+		transform.position = new_pos;
+	}
+}
---
+++ Assets/scripts/UnitSelection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelection : MonoBehaviour {
+
+    bool isSelecting = false;
+    Vector3 mousePosition1;
+    
+ 
+	public bool IsWithinSelectionBounds( GameObject gameObject )
+    {
+        if( !isSelecting )
+            return false;
+ 
+        var camera = Camera.main;
+        var viewportBounds =
+            DrawUtils.GetViewportBounds( camera, mousePosition1, Input.mousePosition );
+            
+        return viewportBounds.Contains(
+            camera.WorldToViewportPoint( gameObject.transform.position ) );
+    }
+
+    void Update()
+    {
+        // If we press the left mouse button, save mouse location and begin selection
+        if( Input.GetMouseButtonDown( 0 ) )
+        {
+            isSelecting = true;
+            mousePosition1 = Input.mousePosition;
+        }
+        // If we let go of the left mouse button, end selection
+        if( Input.GetMouseButtonUp( 0 ) )
+            isSelecting = false;
+    }
+ 
+    void OnGUI()
+    {
+        if( isSelecting )
+        {
+            // Create a rect from both mouse positions
+            var rect = DrawUtils.GetScreenRect( mousePosition1, Input.mousePosition );
+            DrawUtils.DrawScreenRect( rect, new Color( 0.8f, 0.8f, 0.95f, 0.25f ) );
+            DrawUtils.DrawScreenRectBorder( rect, 2, new Color( 0.8f, 0.8f, 0.95f ) );
+        }
+    }
+}
